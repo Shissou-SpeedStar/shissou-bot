f05de50ced1ec42615f7cb5739c94cf18ee56b32
Shissou-SpeedStar <tamgamecreator.3rd@gmail.com>
2025-10-17 23:40:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,35 @@ tree = app_commands.CommandTree(client)
 JST = timezone(timedelta(hours=9))
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")  # 安全な方法で読み込む（本番環境ではSecrets管理推奨）
+@client.event
+async def on_guild_join(guild):
+    if guild.id not in ALLOWED_GUILD_IDS:
+        print(f"❌ 許可されていないサーバー ({guild.name}) に参加したため退出します。")
+        try:
+            await guild.leave()
+        except Exception as e:
+            print(f"⚠️ サーバーから退出できませんでした: {e}")
+    else:
+        print(f"✅ 許可されたサーバー ({guild.name}) に参加しました。")
 
-                
+@client.event
+async def on_ready():
+    print('ログインしました')
+ # アクティビティを設定
+    activity = discord.Activity(name='疾風スピードスター', type=discord.ActivityType.competing)
+    await client.change_presence(status=discord.Status.online, activity=activity)
+    # スラッシュコマンドを同期
+    await tree.sync()
+
+#スラッシュコマンド
+@tree.command(name='membercount', description='サーバーの人数を表示します') 
+async def member_count(message):
+    # message インスタンスから guild インスタンスを取得
+    guild = message.guild 
+    # ユーザとBOTを区別しない場合
+    member_count = guild.member_count
+    await message.response.send_message(f'今の人数は{member_count}です')
+
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
 keep_alive()
