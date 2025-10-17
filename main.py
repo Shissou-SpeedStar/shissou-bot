0beea55cf848bea016eb86f81bc9c56e9c21ab75
Shissou-SpeedStar <tamgamecreator.3rd@gmail.com>
2025-10-18 00:36:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,46 @@ import discord
 import os
 from keep_alive import keep_alive
 
-client = discord.Client(intents=discord.Intents.default())
+intents=discord.Intents.all()
+intents.message_content = True
+intents.members = True  # メンバー参加イベントを取得するために必要
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
+ALLOWED_GUILD_IDS = {1235503983179730944,1268381411904323655,1268199427865055345}  # ✅ Botが所属できるサーバーIDをここに記入（複数対応可）
+# 日本時間（JST）
+JST = timezone(timedelta(hours=9))
+
+@client.event
+async def on_guild_join(guild):
+    if guild.id not in ALLOWED_GUILD_IDS:
+        print(f"❌ 許可されていないサーバー ({guild.name}) に参加したため退出します。")
+        try:
+            await guild.leave()
+        except Exception as e:
+            print(f"⚠️ サーバーから退出できませんでした: {e}")
+    else:
+        print(f"✅ 許可されたサーバー ({guild.name}) に参加しました。")
 @client.event
 async def on_ready():
     print('ログインしました')
+
+#スラッシュコマンド
+@tree.command(name='membercount', description='サーバーの人数を表示します') 
+async def member_count(message):
+    # message インスタンスから guild インスタンスを取得
+    guild = message.guild 
+    # ユーザとBOTを区別しない場合
+    member_count = guild.member_count
+    await message.response.send_message(f'今の人数は{member_count}です')
+@tree.command(name='BotBoot', description='botを起動します') 
+async def bot_boot(message):
+    url = "https://shissou-bot.onrender.com/"
+    try:
+        response = requests.get(url)
+        await message.response.send_message("起動完了")
+    except requests.exceptions.RequestException as e:
+        await message.response.send_message("エラーが発生しました:", e)
 
 @client.event
 async def on_message(message):
