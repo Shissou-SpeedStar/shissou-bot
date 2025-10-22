d4c449a8e523c349be1ee597bdf031f741f784e8
Shissou-SpeedStar <tamgamecreator.3rd@gmail.com>
2025-10-23 00:30:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,15 @@ JST = timezone(timedelta(hours=9))
 
 # --- User-Agent を設定 ---
 HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/119.0.0.0 Safari/537.36"
-    )
-}
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/125.0.0.0 Safari/537.36"
+        ),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "ja,en-US;q=0.9,en;q=0.8",
+        "Connection": "keep-alive",
+    }
 
 # --- イベント ---
 @client.event
